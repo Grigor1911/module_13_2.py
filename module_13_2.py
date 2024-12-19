231c7d3e84a297f909bca59c5c192bdcbdb5b1f4
Grigor1911 <grixan.1941@mail.ru>
2024-12-19 22:34:19 +0500

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+from pyexpat.errors import messages
+
+api = ""
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+@dp.message_handler(text=["/start"])
+async def start_message(message):
+    print("Привет! Я Бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_messages(message):
+    print("Введите команду /start, чтобы начать общение")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
